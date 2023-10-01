a2d39ce86ed5b22029bb0af18a24ab97d999d7bb
Konstantinos Koutros <koutrosk@unisystems.gr>
2023-10-01 15:07:36 +0300

    Git init

---
+++ src/main/java/gr/accepted/gamematch/GameMatchApplication.java
@@ -0,0 +1,13 @@
+package gr.accepted.gamematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameMatchApplication.class, args);
+	}
+
+}
